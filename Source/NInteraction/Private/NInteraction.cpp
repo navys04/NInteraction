b82583cbc037fbf585e513e35b994c04762c25ea
sarmatvashaev2004 <sarmatvashaev2004@gmail.com>
2025-05-07 05:39:11 +0300

    Plugin initialize

---
+++ Source/NInteraction/Private/NInteraction.cpp
@@ -0,0 +1,20 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "NInteraction.h"
+
+#define LOCTEXT_NAMESPACE "FNInteractionModule"
+
+void FNInteractionModule::StartupModule()
+{
+	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+}
+
+void FNInteractionModule::ShutdownModule()
+{
+	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
+	// we call this function before unloading the module.
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FNInteractionModule, NInteraction)
